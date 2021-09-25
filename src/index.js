902c964ebfe71e6a8e2c150701974471d6f74825
Charbel Mahfouz <charbel.mahfouz52@gmail.com>
2021-09-25 14:41:51 +0300

    fixing ssl

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import "./i18n"
 import { Provider } from "react-redux"
 
 import store from "./store"
+if (window.location.protocol.indexOf("https") == 0) {
+  var el = document.createElement("meta")
+  el.setAttribute("http-equiv", "Content-Security-Policy")
+  el.setAttribute("content", "upgrade-insecure-requests")
+  document.head.append(el)
+}
 
 const app = (
   <Provider store={store}>
